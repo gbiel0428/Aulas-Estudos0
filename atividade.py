2c4b2af51104f2d963366e5c0c673e8dd51d007c
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-15 16:38:18 -0300

    feat tipagem e operacoes matematicas

---
+++ atividade.py
@@ -0,0 +1,17 @@
+
+import os
+os.system("cls")
+
+
+valor_1 = float(input("Digite o valor_1: "))
+valor_2 = float(input("Digite o valor_2: "))
+
+soma = valor_1 + valor_2
+subtracao = valor_1 - valor_2
+multiplicacao = valor_1 * valor_2
+divisao = valor_1 / valor_2
+
+print(f"A soma é: {soma}")
+print(F"A subtracao é:{subtracao}")
+print(F"A multiplicacao é: {multiplicacao}")
+print(F"A divisao é: {divisao}")
---
+++ tipagem.py
@@ -0,0 +1,13 @@
+#Tipagem dinamica.
+
+#limpar o terminal.
+import os
+os.system("cls")
+
+
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+print(f"nome: {nome}")
+print(f"idade: {idade}")
