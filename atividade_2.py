89f7625ecbd835faa145ae5bbfa44ee8a32a1272
gbiel0428 <gabriiiel2807@gmail.com>
2025-08-22 16:51:59 -0300

    Create atividade_2.py

---
+++ atividade_2.py
@@ -0,0 +1,19 @@
+import os
+os.system("cls")
+
+
+valor_1 = float(input("Digite o valor_1: "))
+valor_2 = float(input("Digite o valor_2: "))
+
+soma = valor_1 + valor_2
+media = valor_1 + valor_2 / 2
+produto = valor_1 * valor_2
+Menorvalor = min(valor_1,valor_2)
+maiorvalor = max(valor_1,valor_2)
+
+print(f"A soma é: {soma}")
+print(F"A media é: {media}")
+print(f"O produto é: {produto}")
+print(f"O menor valor é {Menorvalor}")
+print(f"O maior valor é {maiorvalor}")
+
